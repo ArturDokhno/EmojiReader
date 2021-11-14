a2c1c68e1e57615d5c7f0077f8640dcc09e7595e
ArturDokhno <artur.dohno@icloud.com>
2021-11-14 17:49:33 +0500

    Добавили специальные кнопки к ячейки. Первая кнопка удаление, вторая кнопка устанавливает значение фаворит для ячейки.

--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -55,8 +55,39 @@ class EmojiTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let movedEmoji = objects.remove(at: sourceIndexPath.row) // сохраняем удаленый элемент из массива в константу
-        objects.insert(movedEmoji, at: destinationIndexPath.row) // добавляем удаленый элемент в массив в нужном месте
-        tableView.reloadData() // перезагружаем таблицу
+        objects.insert(movedEmoji, at: destinationIndexPath.row) // добавляем удаленый элемент в массив в нужном месте таблицы
+        tableView.reloadData() // перезагружаем таблицу для отображения перемещения
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let done = donection(at: indexPath) // создаем константу в которую передаем функциию с работой по индуксу ячейки
+        let fovourite = fovouriteAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [done, fovourite]) 
+    }
+    
+    func donection(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Done") {
+            (action, view, completion) in
+            self.objects.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true) // указываем что дейсвие с кнопкой завершается
+        }
+        action.backgroundColor = .systemGreen // устанавливем цвет кнопки
+        action.image = UIImage(systemName: "checkmark.circle") // устанавливаем картинку кнопки
+        return action
+    }
+    
+    func fovouriteAction( at indexPath: IndexPath) -> UIContextualAction {
+        var object = objects[indexPath.row] // достаем обьект из массива
+        let action = UIContextualAction(style: .normal, title: "Favourite") {
+            (action, view, completion) in
+            object.isFavorite = !object.isFavorite // меняем его свойство на обратное значение
+            self.objects[indexPath.row] = object // заменяем в массиве элемент на новый измененный
+            completion(true)
+        }
+        action.backgroundColor = object.isFavorite ? .systemPurple : .systemGray // меняем цвет в зависимости от значения isFavorite
+        action.image = UIImage(systemName: "heart")
+        return action
     }
     
 }
