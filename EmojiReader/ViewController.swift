c1dc5e278bc497adc75eb8427292c84d4c9b78a7
ArturDokhno <artur.dohno@icloud.com>
2021-11-14 13:10:25 +0500

    Initial Commit

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Артур Дохно on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
